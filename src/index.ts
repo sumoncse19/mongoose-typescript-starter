abd35ff884c3335ce652392855a6a1fc714649ba
Md. Sumon <72946136+sumoncse19@users.noreply.github.com>
2023-12-05 03:58:13 +0600

    Setup express and connect with database

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,34 @@
-console.log('Hello TypeScript')
+import express, { Application } from 'express';
+import cors from 'cors'
+import 'dotenv/config'
+import mongoose from 'mongoose';
+
+const app: Application = express();
+
+// using cors
+app.use(cors());
+
+// parse data
+app.use(express.json());
+app.use(express.urlencoded({ extended: false })); // false --> express app sudho matro string and object k receive korbe  // true --> express app any type receive korbe
+
+const { URI } = process.env
+const port = process.env.PORT || 5000;
+
+const dbConnect = async (): Promise<void> => {
+  if (!URI) {
+    throw new Error('URL is not specified')
+  }
+  await mongoose.connect(URI).then(() => console.log('Database Connected!'));
+}
+
+dbConnect()
+
+app.get('/', (req, res) => {
+  res.send({
+    message: 'Our server is running',
+    status: 200,
+  })
+})
+
+app.listen(port, () => console.log(`Server is running at http://localhost:${port}`))
